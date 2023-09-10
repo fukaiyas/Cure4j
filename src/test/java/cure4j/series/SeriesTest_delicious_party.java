a3b71d02bd3374f44881e1d5d2146f28434aa38b
fukaiyas <yasuyuki.fukai@gmail.com>
2023-09-10 10:42:45 +0900

    End of Delicious party precure.

--- src/test/java/cure4j/series/SeriesTest_delicious_party.java
+++ src/test/java/cure4j/series/SeriesTest_delicious_party.java
@@ -18,7 +18,7 @@ public class SeriesTest_delicious_party {
         assertEquals("delicious_party", delicious_party.seriesName);
         assertEquals("デリシャスパーティ♥プリキュア", delicious_party.title);
         assertEquals(LocalDate.of(2022, 2, 6), delicious_party.startedDate);
-//        assertEquals(LocalDate.of(2023, 1, 29), delicious_party.endedDate);
+        assertEquals(LocalDate.of(2023, 1, 29), delicious_party.endedDate);
         assertEquals(4, delicious_party.girls.size());
         assertTrue(delicious_party.girls.contains(Cure.precious));
         assertTrue(delicious_party.girls.contains(Cure.spicy));
@@ -34,11 +34,11 @@ public class SeriesTest_delicious_party {
     void onAir(){
         assertFalse(delicious_party.isOnAir(LocalDate.of(2022, 2, 5)));
         assertTrue(delicious_party.isOnAir(LocalDate.of(2022, 2, 6)));
-//        assertTrue(delicious_party.isOnAir(LocalDate.of(2023, 1, 29)));
-//        assertFalse(delicious_party.isOnAir(LocalDate.of(2023, 1, 30)));
+        assertTrue(delicious_party.isOnAir(LocalDate.of(2023, 1, 29)));
+        assertFalse(delicious_party.isOnAir(LocalDate.of(2023, 1, 30)));
 
         DateUtil.setDefaultCurrentDate();
-        assertTrue(delicious_party.isOnAir());
+        assertFalse(delicious_party.isOnAir());
 
         DateUtil.setCurrentDate(LocalDate.of(2022, 7, 14));
         assertTrue(delicious_party.isOnAir());
